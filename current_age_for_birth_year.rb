d38850d2b1fd2377fba886241c9759931165322e
Johana Gutierrez <johagutierrezg@gmail.com>
2020-04-23 02:39:13 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,4 @@
-def current_age_for_birth_year(1984)
-  age_of_person = "2003 - #{current_age_for_birth_year}"
-  return = 19
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
 end
-  
- current_age_for_birth_year
  
